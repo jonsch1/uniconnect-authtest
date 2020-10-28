8aae4b75e8aa579232b0afd9d98ffbfd149adbc7
jonathanschaul <j.schaul@student.maastrichtuniversity.nl>
2020-10-28 10:40:17 +0100

    main

--- src/oidcsettings.js
+++ src/oidcsettings.js
@@ -1,8 +1,8 @@
 var OidcSettings = {    
-    authority: 'https://****/identity',
-    client_id: 'myclientid',
-    redirect_uri: 'https://localhost:9090/',    
+    authority: 'https://connect.test.surfconext.nl/oidc/authorize',
+    client_id: 'uniconnect-authtest.netlify.app/',
+    redirect_uri: 'https://uniconnect-authtest.netlify.app/',    
     response_type: 'id_token token',
     scope: 'openid profile roles',
-    post_logout_redirect_uri: 'https://localhost:9090/'      
+    post_logout_redirect_uri: 'https://uniconnect-authtest.netlify.app/logout'      
 };
